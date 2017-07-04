655c533c730b6a2dfc28bf1815f124e2eca0680e
o002073 <Ryan.J.Newsom@kp.org>
2017-07-04 14:42:27 -0600

    Added ng style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,10 +5,20 @@ import { Component } from "@angular/core";
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus:string = 'offline';
+
   getServerStatus() {
     return "Good to go";
   }
 
-  serverId = 10;
-  serverStatus:string = 'offline';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+
 }
