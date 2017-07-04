7e458d2cd8d996496178a81d07ced310c3ea46b1
o002073 <Ryan.J.Newsom@kp.org>
2017-07-04 14:36:31 -0600

    Added ng if else

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreated = false;
   serverCreationStatus = "No server was created";
   serverName = "Testserver";
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -20,6 +22,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created. Name is " + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: any) {
